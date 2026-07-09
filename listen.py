0e942172024b2a58336e190af393dec4ec0ab3d5
thenehabisht <thenehabisht13@gmail.com>
2026-07-09 12:39:17 +0530

    fixed code

--- listen.py
+++ listen.py
@@ -4,7 +4,7 @@ def listen():
 
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
-
+        print("Listening...") 
         recognizer.adjust_for_ambient_noise(source, duration=0.5)
 
         audio = recognizer.listen(source)
@@ -12,6 +12,7 @@ def listen():
 
     try:
         text = recognizer.recognize_google(audio)
+        print("You said:", text)
         return text
 
 
